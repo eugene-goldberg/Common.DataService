7aa430e2dece169f95cfee64be9e49d4033e891c
Eugene Goldberg <eugene.goldberg117@gmail.com>
2016-05-10 11:01:12 -0500

    added the Kafka consumer code into Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,86 @@
 ﻿using System;
 using System.ServiceProcess;
+using System.Collections.Generic;
 using Microsoft.Owin.Hosting;
 using Topshelf;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace SelfHostedWebApiDataService
 {
     
     class Program
     {
+        public class MyModel
+        {
+            public  string INCIDENTNUMBER { get; set; }
+            public  string SUBMITTER { get; set; }
+            public  DateTime REPORTEDDATE { get; set; }
+            public  DateTime LASTRESOLVEDDATE { get; set; }
+            public  string OWNERGROUP { get; set; }
+            public  string COMPANY { get; set; }
+            public  string CATEGORIZATIONTIER1 { get; set; }
+            public  string VATEGORIZATIONTIER2 { get; set; }
+            public  string CATEGORIZATIONTIER3 { get; set; }
+            public  string RESOLUTIONCATEGORY { get; set; }
+            public  string RESOLUTIONCATEGORYTIER2 { get; set; }
+            public  string RESOLUTIONCATEGORYTIER3 { get; set; }
+            public  string REPORTEDSOURCE { get; set; }
+            public  string DESCRIPTION { get; set; }
+        }
+        public static async Task KafkaConsumerAsync()
+            {
+             string s1 = await ReadKafkaMessagesAsync();
+             Console.WriteLine("KafkaConsumerAsync()");
+            }
+
+        private static Task<string> ReadKafkaMessagesAsync()
+        {
+            return Task<string>.Factory.StartNew(() => ReadKafkaMessages());
+        }
+
+        private static string ReadKafkaMessages()
+        {
+            var options = new KafkaOptions(new Uri("http://13.89.38.33:9092"), new Uri("http://13.89.38.33:9092"));
+            var router = new BrokerRouter(options);
+
+            var consumerOffset = new Consumer(new ConsumerOptions("ora1", router));
+
+            var offsets = consumerOffset.GetTopicOffsetAsync("ora1").Result
+                   .Select(x => new OffsetPosition(x.PartitionId, x.Offsets.Max())).ToArray();
+
+            var consumer = new Consumer(new ConsumerOptions("ora1", router), offsets);
+
+            //Consume returns a blocking IEnumerable (ie: never ending stream)
+            foreach (var message in consumer.Consume())
+            {
+                string stringValue = System.Text.Encoding.Default.GetString(message.Value);
+
+                var models = JsonConvert.DeserializeObject<IList<MyModel>>(stringValue);
+
+                foreach (MyModel model in models)
+                {
+                    Debug.WriteLine("MODEL:");
+                    Debug.WriteLine(model.INCIDENTNUMBER);
+                    Debug.WriteLine(model.SUBMITTER);
+                    Debug.WriteLine(model.REPORTEDDATE);
+                    Debug.WriteLine(model.OWNERGROUP);
+                    Debug.WriteLine("MODEL END:");
+                    Debug.WriteLine("\n\n");
+                }
+            }
+
+            return null;
+        }
         static void Main(string[] args)
         {
-            //StartConsole();
-            //StartServiceBase();
+            StartConsole();
+
             StartTopshelf();
         }
 
@@ -33,10 +102,12 @@ namespace SelfHostedWebApiDataService
             });
         }
 
-        static void StartConsole()
+        static async void StartConsole()
         {
+            await KafkaConsumerAsync();
+
             using (WebApp.Start<SelfHostedWebApiDataService.Startup>("http://*:8888"))
-            {
+            {                
                 Console.WriteLine("Web Server is running.");
                 Console.WriteLine("Press any key to quit.");
                 Console.ReadLine();
